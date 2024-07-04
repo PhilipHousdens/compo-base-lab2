f5e02c61c8242816dc865d1b8d32adfbfc96ba44
Philip Housden <philip.houden.04@gmail.com>
2024-07-04 19:03:27 +0700

    question 15 done

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -3,13 +3,12 @@ import EventCard from '@/components/EventCard.vue'
 import CategoryOrganizer from '@/components/CategoryOrganizer.vue'
 import Event from '@/types/Event'
 import { ref, onMounted } from 'vue'
-import axios from 'axios'
+import EventService from '@/services/EventService'
 
 const events = ref<Event[]>(null)
 
 onMounted(() => {
-  axios
-    .get('https://my-json-server.typicode.com/PhilipHousdens/compo-base-demo-server/events')
+  EventService.getEvents()
     .then((response) => {
       events.value = response.data
     })
