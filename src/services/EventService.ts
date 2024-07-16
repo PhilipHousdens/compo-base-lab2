f15f5d1f2303ceebc4adbb1d85ae324d8a924f39
Philip Housden <philip.houden.04@gmail.com>
2024-07-16 21:48:46 +0700

    added EventDetailView.vue

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -12,5 +12,8 @@ const apiClient = axios.create({
 export default {
     getEvents() {
         return apiClient.get('/events')
+    },
+    getEventById(id: number) {
+        return apiClient.get('events/' + id.toString)
     }
 }
---
+++ src/views/EventDetailView.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import EventService from '@/services/EventService';
+import {EventItem} from '@/type'
+const event = ref<EventItem | null>(null)
+const id: Ref<number> = ref(123)
+
+EventService.getEventById(id.value)
+    .then((response) => {
+        event.value = response.data
+    }).catch(error => {
+        console.log(error)
+    })
+</script>
+<template>
+    <div v-if="event">
+        <h1>{{event.title}}</h1>
+        <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+        <p>{{ event.description }}</p>
+    </div>
+</template>
+<style lang="">
+    
+</style>
