fd57cd212a1dc0dc13d426345fcff7a0c271e192
Philip Housden <philip.houden.04@gmail.com>
2024-07-29 20:59:04 +0700

    q 2 done

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,7 @@ import EventRegisterView from '@/views/event/EventRegisterView.vue'
 import EventLayoutView from '@/views/event/EventLayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/NetworkErrorView.vue'
+import nProgress from 'nprogress'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -71,8 +72,15 @@ const router = createRouter({
           props: true
         },
       ]
-    }
+    },
   ]
+})
+router.beforeEach(() => {
+  nProgress.start()
+})
+
+router.afterEach(() => {
+  nProgress.done()
 })
 
 export default router
