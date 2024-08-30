d821ccd95364a993d77e0bedf681c9c0173135d7
Philip Housden <philip.houden.04@gmail.com>
2024-08-30 11:14:07 +0700

    Change the location of backend

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/PhilipHousdens/compo-base-demo-server',
+  baseURL: 'http://localhost:8080',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
