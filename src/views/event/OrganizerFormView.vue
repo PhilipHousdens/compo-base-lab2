e91a3942758a4ea4c9cf258b53156f7747cbfeda
Philip Housden <philip.houden.04@gmail.com>
2024-10-10 15:49:54 +0700

    added style for Organizer

--- src/views/event/OrganizerFormView.vue
+++ src/views/event/OrganizerFormView.vue
@@ -28,14 +28,17 @@ function saveEvent() {
 }
 </script>
 <template>
-    <div>
+    <div class="flex justify-center flex-col items-center">
         <h1>Create an Organizer</h1>
-        <form @submit.prevent="saveEvent" >
+        <form @submit.prevent="saveEvent" class=" bg-green-400 p-5 w-[35%] rounded-t-lg space-y-4 space-x-2 text-lg " >
             <h3>Organizer Name</h3>
             <BaseInput v-model="organizer.name" label="Organizer"/>
             <ImageUpload v-model="organizer.images"/>
             <button class="button" type="submit">Submit</button>
         </form>
-    <pre>{{ organizer }}</pre>
+        <div  class=" bg-black p-10 w-[35%] bg-opacity-50 rounded-b-lg text-white">
+            <pre>{{ organizer }}</pre>
+        </div>
+    
     </div>
 </template>
